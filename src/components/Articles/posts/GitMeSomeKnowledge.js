2815ca3d9095221881a51aadec0cc059d8b911f1
bishaludas <supertramp.bishal@gmail.com>
2021-12-05 22:31:54 +0545

    articles update

--- src/components/Articles/posts/GitMeSomeKnowledge.js
+++ src/components/Articles/posts/GitMeSomeKnowledge.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import Typography from "@material-ui/core/Typography";
 import Chip from "@material-ui/core/Chip";
 import { makeStyles } from "@material-ui/core/styles";
+import GitMeSomeKnowledge1 from "./images/GitMeSomeKnowledge1.png";
 
 const GitMeSomeKnowledge = () => {
   let tags = ["git"];
@@ -37,31 +38,74 @@ const GitMeSomeKnowledge = () => {
 
       {/*content*/}
       <article>
-        <Typography variant="body2" component="div" gutterBottom>
+        <p>
           Due to covid-19, I had been working from home (wfh) for almost a year
           and using my personal laptop for office works. At the beginning of wfh
           , I remembering setting up my office email for setting up git config
-          globally. git config --global user.name "Your global username" git
-          config --global user.email "office email" Things were working okay,
-          and after some months I began working on some side projects, made some
-          commits and pushed it to GitHub. Then i went to check into GitHub on
-          repo status and noticed that my timeline was not reflecting any
-          commits. From November to early January, the commits timeline was
-          empty. I searched some posts over stack overflow google and found out
-          the cause for this was my git's globally configured email was
-          different than that of GitHub. Found out Git offers other ways on how
-          you can set you username and email. project - The data is stored
-          directly in the corresponding project(repository) under .git/config.
-          global - for all repositories of the current user. The config file can
-          be found in your home directory ~/gitconfig. system - This is a system
-          wide configuration and is available for all users and all
-          repositories. You will find it in /etc/gitconfig. In my case, I set my
-          GitHub's email as global config value and used work email for work
-          specific configuration.Set the username/email for a specific
-          repository # change directory to project directory and run command
-          below cd project/directory git config user.name "Your project specific
-          name" git config user.email "your@project-specific-email.com"
-        </Typography>
+          globally.
+        </p>
+
+        <pre class="ql-syntax" spellcheck="false">
+          git config --global user.name "Your global username" <br />
+          git config --global user.email "office email"
+        </pre>
+        <p>
+          <br />
+        </p>
+        <p>
+          Things were working okay, and after some months I began working on
+          some side projects, made some commits and pushed it to GitHub. Then i
+          went to check into GitHub on repo status and noticed that my timeline
+          was not reflecting any commits.
+        </p>
+        <p>
+          <img src={GitMeSomeKnowledge1} alt="Github" />
+        </p>
+        <p></p>
+        <p>
+          <br />
+        </p>
+        <p>
+          From November to early January, the commits timeline was empty. I
+          searched some posts over stack overflow &amp; google and found out the
+          cause for this was my git's globally configured email was different
+          than that of GitHub.
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          Found out Git offers other ways on how you can set you username and
+          email.
+        </p>
+        <ol>
+          <li>
+            project - The data is stored directly in the corresponding
+            project(repository) under .git/config.
+          </li>
+          <li>
+            global - for all repositories of the current user. The config file
+            can be found in your home directory ~/gitconfig.
+          </li>
+          <li>
+            system - This is a system wide configuration and is available for
+            all users and all repositories. You will find it in /etc/gitconfig.
+          </li>
+        </ol>
+        <p>
+          <br />
+        </p>
+        <p>
+          In my case, I set my GitHub's email as global config value and used
+          work email for work specific configuration.Set the username/email for
+          a specific repository
+        </p>
+        <pre class="ql-syntax" spellcheck="false">
+          # change directory to project directory and run command below <br />
+          cd &lt;project/directory&gt; <br />
+          git config user.name "Your project specific name" <br />
+          git config user.email "your@project-specific-email.com"
+        </pre>
       </article>
     </Fragment>
   );
